a1ba1a6cc42a38718db92559b2785deccf53d96f
Zaid Kamdar <zkamdar@ad.unc.edu>
2022-08-22 23:23:42 -0400

    Demonstates asking user for input

---
+++ lessons/input.py
@@ -0,0 +1,6 @@
+"""Demonstrates asking the user for input"""
+
+user_name: str = input("What is you name? ")
+print("Hello, " + user_name + ", good morning!")
+print("You are awesome, " + user_name + "!")
+
