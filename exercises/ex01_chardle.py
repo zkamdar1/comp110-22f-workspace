d4266aa7cdff8854a872a96445a609364930e735
Zaid Kamdar <zkamdar@ad.unc.edu>
2022-08-25 21:53:37 -0400

    Fix syntax issues and type errors

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,7 +3,7 @@
 __author__ = "730476042"
 
 
-chosen_word : str = str(input("Enter a 5-character word: "))
+chosen_word: str = str(input("Enter a 5-character word: "))
 
 if len(chosen_word) > 5:
     print("Error: Word must contain 5 characters")
@@ -14,7 +14,7 @@ else:
         exit()
 
 
-letter_choice : str = str(input("Enter a single character: "))
+letter_choice: str = str(input("Enter a single character: "))
 
 if len(letter_choice) > 1:
     print("Error: Character must be a single character.")
@@ -49,17 +49,15 @@ if letter_choice == chosen_word[4]:
     print(letter_choice + " found at index 4")
     count = count + 1
 
-count = str(count)
 
-if count == "0":
+if count == 0:
     print("No instances of " + letter_choice + " found in " + chosen_word)
 else:
-    if count == "1":
-        print("1 instance of " + letter_choice + " found in "+ chosen_word)
+    if count == 1:
+        print("1 instance of " + letter_choice + " found in " + chosen_word)
     else:
-        if count == "2":
-            print("2 instances of " + letter_choice + " found in "+ chosen_word)
+        if count == 2:
+            print("2 instances of " + letter_choice + " found in " + chosen_word)
         else:
-            if count > "2":
-                print(count + " instances of " + letter_choice + " found in "+ chosen_word)
-
+            if count > 2:
+                print(str(count) + " instances of " + letter_choice + " found in " + chosen_word)
