d2b61e30014a2fc8e46f621b13e04f479d17f11b
Zaid Kamdar <zkamdar@ad.unc.edu>
2022-09-04 19:59:00 -0400

    Check indices for correct matches

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -4,11 +4,40 @@ __author__ = "730476042"
 
 secret_word: str = ("python")
 
-user_guess: str = str(input("What is your 6-letter guess? "))
+user_guess: str = str(input(f"What is your {len(secret_word)}-letter guess? "))
 
-while len(user_guess) != 6:
-    new_guess: str = str(input("That was not 6 letters! Try again: "))
+while len(user_guess) != len(secret_word):
+    new_guess: str = str(input(f"That was not {len(secret_word)} letters! Try again: "))
     user_guess = new_guess
+
+#Above-asking user for guess
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+i: int = 0
+guess_result = ""
+
+while i < len(secret_word):
+    if user_guess[i] == secret_word[i]:
+        guess_result = guess_result + (f"{GREEN_BOX}")
+    else:
+        guess_result = guess_result + (f"{WHITE_BOX}")
+    i = i + 1
+
+print(guess_result)
+
+#Above- Indexing input and finding matching characters with secret word, outputing corresponding colored box. 
+
+#matching_letter = False
+#alter_indices = 0
+
+#while matching_letter == False and alter_indices < len[secret_word]:
+ #   if alter_indices == user_guess:
+ #      matching_letter = True
+
+
 
 if user_guess == secret_word:
     print("Woo! You got it!")
