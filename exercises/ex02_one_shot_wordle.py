2ba55c4bab7e9495ff4637db31a93b508f908d30
Zaid Kamdar <zkamdar@ad.unc.edu>
2022-09-05 12:08:45 -0400

    Does everything but show yellow box in right spot

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,8 +2,6 @@
 
 __author__ = "730476042"
 
-from curses.ascii import alt
-
 
 secret_word: str = ("python")
 
@@ -13,7 +11,7 @@ while len(user_guess) != len(secret_word):
     new_guess: str = str(input(f"That was not {len(secret_word)} letters! Try again: "))
     user_guess = new_guess
 
-#Above-asking user for guess
+# Above-asking user for guess
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
@@ -28,22 +26,22 @@ while i < len(secret_word):
     if user_guess[i] == secret_word[i]:
         guess_result = guess_result + (f"{GREEN_BOX}")
     else:
-        while matching_letter == False and secret_index < len(secret_word):
+        while matching_letter is False and secret_index < len(secret_word):
             if user_guess[i] == secret_word[secret_index]:
                 matching_letter = True
             else: 
                 secret_index = secret_index + 1
-        if matching_letter == True:
+        if matching_letter is True:
             guess_result = guess_result + (f"{YELLOW_BOX}")
             matching_letter = False
         else:
             guess_result = guess_result + (f"{WHITE_BOX}")
     i = i + 1
 
-#Above- Indexing input and finding matching characters with secret word, outputing corresponding colored box. 
+# Above- Indexing input and finding matching characters with secret word, outputing corresponding colored box. 
 
 print(guess_result)
 if user_guess == secret_word:
     print("Woo! You got it!")
 else:
-    print("Not quite. Play again soon!" )
+    print("Not quite. Play again soon!")
