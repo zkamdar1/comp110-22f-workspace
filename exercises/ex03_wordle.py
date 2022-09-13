15216ca0946ed8c1343882a6f02f9473c5ad89bc
Zaid Kamdar <zkamdar@ad.unc.edu>
2022-09-13 14:10:29 -0400

    Emojified - returns string of emojis for matching

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,6 +2,9 @@
 
 __author__ = "730476042"
 
+from itertools import count
+
+
 def contains_char(string: str, character: str) -> bool:
     """Returns True if character is found in string."""
 
@@ -18,5 +21,26 @@ def contains_char(string: str, character: str) -> bool:
         return True
     else:
         return matching_letter
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+def emojified(guess: str, secret:str) -> str:
+    """Returns a string of emojis based on matching characters."""
     
-            
+    assert len(guess) == len(secret)
+    i: int = 0
+    boxes: str = ""
+    
+    while i < len(secret):
+        if guess[i] == secret[i]:
+            boxes = boxes + (f"{GREEN_BOX}")
+        else:
+            if contains_char(secret, guess[i]) == True:
+                boxes = boxes + (f"{YELLOW_BOX}")
+            else:
+                boxes = boxes + (f"{WHITE_BOX}")
+        i = i + 1
+    return(boxes)
+    
