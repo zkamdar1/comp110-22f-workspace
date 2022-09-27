c7b39774ee41136e3311c6135597260b16f9f7d7
zkamdar1 <zkamdar@ad.unc.edu>
2022-09-26 22:38:18 -0400

    Fixed styling errors

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,9 +2,9 @@
 
 __author__ = "730476042"
 
+
 def only_evens(x: list[int]) -> list:
     """Returns only the even intergers found in given list."""
-
     i: int = 0
     even = []
 
@@ -14,9 +14,9 @@ def only_evens(x: list[int]) -> list:
         i += 1
     return even
 
+
 def concat(xa: list[int], xb: list[int]) -> list:
     """Returns a list containing first list followed by second list."""
-    
     i: int = 0
     new: list[int] = []
 
@@ -31,26 +31,23 @@ def concat(xa: list[int], xb: list[int]) -> list:
         counter += 1
     return new
 
+
 def sub(za: list[int], start: int, end: int) -> list:
-    """Returns in."""
-
-
+    """Returns a list with values from start to end depending on end and start value."""
     sub_list: list[int] = []
 
-    if len(za) == 0 or start > len(za) or end <= 0:
+    if len(za) == 0 or start >= len(za) or end <= 0:
         return sub_list
-
     if end > len(za):
         end = len(za) - 1
     else:
         end = end - 1
-
     if start < 0:
         start = 0
-
     sub_list.append(za[start])
 
     i: int = start + 1
+
     while i < end:
         sub_list.append(za[i])
         i += 1
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -2,62 +2,75 @@
 
 __author__ = "730476042"
 
-from utils import only_evens
+
+from utils import only_evens, concat, sub
+
 
 def test_only_evens_empty() -> None:
+    """Test only_evens function with empty list."""
     x: list[int] = []
     assert only_evens(x) == []
 
 
 def test_only_evens_one_item() -> None:
+    """Test only_evens function with one item in list."""
     x: list[int] = [1]
     assert only_evens(x) == []
 
 
 def test_only_evens_many_items() -> None:
+    """Test only_evens function with list."""
     x: list[int] = [1, 2, 3, 4, 5, 6]
     assert only_evens(x) == [2, 4, 6]
 
 
-from utils import concat
-
 def test_concat_empty_lists() -> None:
+    """Test concat function with empty lists."""
     xa: list[int] = []
     xb: list[int] = []
     assert concat(xa, xb) == []
 
+
 def test_concat_one_empty_list() -> None:
+    """Test concat function with one empty list."""
     xa: list[int] = [1, 2, 3]
     xb: list[int] = []
     assert concat(xa, xb) == [1, 2, 3]
 
+
 def test_concat_no_empty_lists() -> None:
+    """Test concat function with no empty list."""
     xa: list[int] = [2, 4, 6]
     xb: list[int] = [1, 3, 5]
     assert concat(xa, xb) == [2, 4, 6, 1, 3, 5]
 
 
-from utils import sub
-
 def test_sub_empty_list() -> None:
+    """Test sub function with empty list."""
     za: list[int] = []
     start: int = 1
     end: int = 2
     assert sub(za, start, end) == []
 
+
 def test_sub_normal_list() -> None:
+    """Test sub function with list."""
     za: list[int] = [10, 20, 30, 40, 50, 60]
     start: int = 1
     end: int = 4
     assert sub(za, start, end) == [20, 30, 40]
 
+
 def test_sub_negative_start() -> None:
+    """Test sub function with negative start value."""
     za: list[int] = [1, 2, 3, 4, 5, 6]
     start: int = -1
     end: int = 3
     assert sub(za, start, end) == [1, 2, 3]
 
+
 def test_sub_greater_end() -> None:
+    """Test sub function with end value greater than len of list."""
     za: list[int] = [2, 4, 6, 8, 10, 12, 14]
     start: int = 2
     end: int = 8
