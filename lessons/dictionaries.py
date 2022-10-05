ce20b09a10aab65f6f899c781ffa59fc3fa52226
zkamdar1 <zkamdar@ad.unc.edu>
2022-10-04 21:30:12 -0400

    Example using for..in loop for dictionaries

--- lessons/dictionaries.py
+++ lessons/dictionaries.py
@@ -30,3 +30,20 @@ is_duke_present: bool = "Duke" in schools
 print(f"is_duke_present: {is_duke_present}")
 
 print(schools)
+
+# Demonstration of dictionary literals
+
+# Empty dictionary literal
+schools = {} # Same as dict()
+print(schools)
+
+# Alternatively, initialize key-value pairs
+schools = {"UNC": 19400, "Duke": 6717, "NCSU": 26150}
+print(schools)
+
+# What happens when a key does not exist?
+# print(schools["UNCC"])
+
+# Example looping over the keys of a dict
+for school in schools:
+    print(f"Key: {school} -> Value: {schools[school]}")
