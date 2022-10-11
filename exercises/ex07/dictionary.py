8be41ab0d6aafa5b9c7f59acf9f944548d3d4619
zkamdar1 <zkamdar@ad.unc.edu>
2022-10-10 20:44:06 -0400

    Define skeleton dictionary functions

---
+++ exercises/ex07/dictionary.py
@@ -0,0 +1,27 @@
+"""Ex-06 - Practice with dictionary functions."""
+
+__author__ = "730476042"
+
+
+def invert(dictionary: dict[str, str]) -> dict[str, str]:
+    """Given a dictionary, returns dictionary with inverted keys and values."""
+    result: dict[str, str] = {}
+
+
+    return result
+
+
+def favorite_color(dictionary: dict[str, str]) -> str:
+    """Given a dictionary of names and favorite colors, returns a str of most common color."""
+    result: str = ""
+
+
+    return result
+
+
+def count(list: list[str]) -> dict[str, int]:
+    """Given a list, will output a dictionary where key is unique value in list and value is the number of times it appears in list."""
+    result: dict[str, int] = {}
+
+
+    return result
