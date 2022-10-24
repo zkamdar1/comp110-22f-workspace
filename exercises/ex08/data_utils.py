50bdaa3e7285230a1b540c3a481d5f5ce0622fb2
zkamdar1 <zkamdar@ad.unc.edu>
2022-10-24 19:52:15 -0400

    Util functions for data wrangling

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -1,5 +1,62 @@
 """Dictionary related utility functions."""
 
-__author__ = ""
+__author__ = "730476042"
 
 # Define your functions below
+
+from csv import DictReader
+from typing import Dict
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a csv into a 'table'."""
+    result: list[dict[str, str]] = []
+
+    # Open a handle to the data file
+    file_handle = open(filename, "r", encoding="utf8")
+
+    # Prepare to read the data file as a CSV
+    csv_reader = DictReader(file_handle)
+
+    # Read each row of the CSV line-by-line
+    for row in csv_reader:
+        result.append(row)
+
+    # Close the file when we're done, to free its resources.
+    file_handle.close()
+
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result
+
+
+def columnar(row__table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oriented table."""
+    result: dict[str, list[str]] = {}
+
+    first_row: dict[str, str] = row__table[0]
+    for column in first_row:
+        result[column] = column_values(row__table, column)
+
+    return result
+
+
+def head(table: dict[str, list[str]], rows: int) -> dict[str, list[str]]:
+    """Produce a column-based table with specific amount of rows."""
+    result: dict[str, list[str]] = {}
+
+    for key in table:
+        item: list[str] = []
+        i: int = 0
+        while i < rows:
+            item.append(key[i])
+            i += 1
+        result[key] = item
+    return result
+
