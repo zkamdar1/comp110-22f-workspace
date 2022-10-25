8384eb86cc6fdce3cd4ed3c45484367e1632880a
zkamdar1 <zkamdar@ad.unc.edu>
2022-10-25 16:24:55 -0400

    Made utility functions for simple data analysis

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -50,13 +50,51 @@ def columnar(row__table: list[dict[str, str]]) -> dict[str, list[str]]:
 def head(table: dict[str, list[str]], rows: int) -> dict[str, list[str]]:
     """Produce a column-based table with specific amount of rows."""
     result: dict[str, list[str]] = {}
-
+    
     for key in table:
-        item: list[str] = []
+        values: list[str] = []
         i: int = 0
         while i < rows:
-            item.append(key[i])
+            key_value = list(table[key])
+            values.append(key_value[i])
             i += 1
-        result[key] = item
+        result[key] = values
     return result
 
+
+def select(main: dict[str, list[str]], names: list[str]) -> dict[str, list[str]]:
+    """Produce a column-based table with specific columns."""
+    result: dict[str, list[str]] = {}
+
+    for key in names:
+        result[key] = main[key]
+
+    return result
+
+
+def concat(table_a: dict[str, list[str]], table_b: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Given two column-based tables, produces a combined table."""
+    result: dict[str, list[str]] = {}
+
+    for key in table_a:
+        result[key] = table_a[key]
+
+    for items in table_b:
+        present: bool = items in result
+        if present is True:
+            result[items] += table_b[items]
+        else:
+            result[items] = table_b[items]
+    return result
+
+
+def count(data: list[str]) -> dict[str, int]:
+    """Returns a dict of the frequencies of items in list."""
+    result: dict[str, int] = {}
+
+    for item in data:
+        if item in result:
+            result[item] += 1
+        else:
+            result[item] = 1
+    return result
