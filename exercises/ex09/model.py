2fd471b6baaedc53f3a5abca917e38f14a9f6ccf
zkamdar1 <zkamdar@ad.unc.edu>
2022-11-12 17:59:35 -0500

    add code to show starting infected cells in model

--- exercises/ex09/model.py
+++ exercises/ex09/model.py
@@ -6,8 +6,7 @@ from exercises.ex09 import constants
 from math import sin, cos, pi
 
 
-__author__ = ""  # TODO
-
+__author__ = "730476042"
 
 class Point:
     """A model of a 2-d cartesian coordinate Point."""
@@ -30,12 +29,13 @@ class Cell:
     """An individual subject in the simulation."""
     location: Point
     direction: Point
-    sickness: int = 0
+    sickness: int = constants.VULNERABLE
 
-    def __init__(self, location: Point, direction: Point):
+    def __init__(self, location: Point, direction: Point, sickness: int):
         """Construct a cell with its location and direction."""
         self.location = location
         self.direction = direction
+        self.sickness = sickness
 
     # Part 1) Define a method named `tick` with no parameters.
     # Its purpose is to reassign the object's location attribute
@@ -47,7 +47,31 @@ class Cell:
     def color(self) -> str:
         """Return the color representation of a cell."""
         return "black"
+    
+    def contract_disease(self) -> None:
+        """Assign infected to sickness attribute."""
+        self.sickness = constants.INFECTED
 
+    def is_vulnerable(self) -> bool:
+        """Return True is sickness is vulnerable."""
+        if self.sickness == constants.VULNERABLE:
+            return True
+        else:
+            return False
+
+    def is_infected(self) -> bool:
+        """Return True if sickness is infected."""
+        if self.sickness == constants.INFECTED:
+            return True
+        else:
+            return False
+        
+    def color(self) -> str:
+        """Return color of cell depending on sickness."""
+        if self.is_vulnerable() == True:
+            return "gray"
+        elif self.is_infected() == True:
+            return "red" 
 
 class Model:
     """The state of the simulation."""
@@ -55,14 +79,26 @@ class Model:
     population: list[Cell]
     time: int = 0
 
-    def __init__(self, cells: int, speed: float):
-        """Initialize the cells with random locations and directions."""
+    def __init__(self, cells: int, speed: float, infected: int):
+        """Initialize the cells with infected, random locations and directions."""
         self.population = []
-        for i in range(cells):
+        if infected >= cells or infected <= 0:
+            raise ValueError("Some # of cells must begin infected.")
+        
+        for i in range(cells - infected):
             start_location: Point = self.random_location()
             start_direction: Point = self.random_direction(speed)
-            cell: Cell = Cell(start_location, start_direction)
+            sick: int = constants.VULNERABLE
+            cell: Cell = Cell(start_location, start_direction, sick) 
             self.population.append(cell)
+        
+        for i in range(infected):
+            start_location: Point = self.random_location()
+            start_direction: Point = self.random_direction(speed)
+            sick: int = constants.INFECTED
+            cell: Cell = Cell(start_location, start_direction, sick)
+            self.population.append(cell)
+
     
     def tick(self) -> None:
         """Update the state of the simulation by one time step."""
