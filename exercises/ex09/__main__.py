b6d442671a0a13cc1a43da90909317e103060dda
zkamdar1 <zkamdar@ad.unc.edu>
2022-11-13 10:47:36 -0500

    add immune cells

--- exercises/ex09/__main__.py
+++ exercises/ex09/__main__.py
@@ -7,7 +7,7 @@ from exercises.ex09.view_controller import ViewController
 
 def main() -> None:
     """Entrypoint of simulation."""
-    model: Model = Model(constants.CELL_COUNT, constants.CELL_SPEED, constants.CELL_INFECTED)
+    model: Model = Model(constants.CELL_COUNT, constants.CELL_SPEED, constants.CELL_INFECTED, constants.CELL_IMMUNE)
     view_controller: ViewController = ViewController(model)
     view_controller.start_simulation()
 
